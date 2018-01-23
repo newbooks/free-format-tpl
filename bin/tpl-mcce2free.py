42cf122492c009efe9da0669fc2df66f1a68ac4c
newbooks <junjun.mao@gmail.com>
2018-01-23 16:22:41 -0500

    1/23/2019

--- bin/tpl-mcce2free.py
+++ bin/tpl-mcce2free.py
@@ -13,6 +13,27 @@ if __name__ == "__main__":
         end = line.find("#")
         line = line[:end]
         if len(line) < 20: continue
-        
-        print line
+        fields = line[:20].split()
+        key1 = fields[0]
+        if len(fields) > 1:
+            key2 = fields[1]
+        else:
+            key2 = ""
+        if len(fields) > 2:
+            key3 = fields[2]
+        else:
+            key3 = ""
+
+        value = line[20:].strip()
+        mccedb[(key1,key2,key3)] = value
+
+    # check consistency between ATOMNAME and IATOM
+    conformers = []
+    for k in mccedb.keys():
+        if k[0] == "CONFLIST":
+            conformers += mccedb[k].split()
+
+
+
+    print conformers
 
